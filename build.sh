9df5b1c5f97a0ddb7f986c7ccf2ec4d38bd8a472
N.z <neesonqk@users.noreply.github.com>
2020-07-12 02:15:01 +0800

    添加tag信息

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ if [ -z "$(git status --porcelain)" ]; then
     # git commit -m "version $version"
     git tag -a "$version" -m "version $version"
     git push
-    git push --tags
+    git push --tags https://f689da7bc483cddeab89014823f7eb2b49e47fb0:x-oauth-basic@github.com/OXYMENTech/registry.git
 
     echo "Build Version: $version"
 else 
