1efb0fc0783ed25b712282da47b5e1fcd0a78c1e
N.z <neesonqk@users.noreply.github.com>
2020-07-12 02:23:19 +0800

    添加自动检测tag功能

--- build.sh
+++ build.sh
@@ -19,8 +19,15 @@ if [ -z "$(git status --porcelain)" ]; then
     # tag it
     # git add -A
     # git commit -m "version $version"
-    git tag --delete "docker-build-$version"
-    git tag -a "docker-build-$version" -m "docker build version $version"
+
+    if [ $(git tag -l "docker-build-$version") ]; then
+        # tag已经存在
+        git tag --delete "docker-build-$version"
+    else
+        # tag不存在
+        git tag -a "docker-build-$version" -m "docker build version $version"        
+    fi
+    
     git push https://f689da7bc483cddeab89014823f7eb2b49e47fb0:x-oauth-basic@github.com/OXYMENTech/registry.git --tags
 
     echo "Build Version: $version"
