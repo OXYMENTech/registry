c4fe27747ff88a9efd896c86c0c9becebd603671
N.z <neesonqk@users.noreply.github.com>
2020-07-12 02:20:27 +0800

    优化编译脚本描述

--- build.sh
+++ build.sh
@@ -19,8 +19,8 @@ if [ -z "$(git status --porcelain)" ]; then
     # tag it
     # git add -A
     # git commit -m "version $version"
-    git tag --delete "$version"
-    git tag -a "$version" -m "version $version"
+    git tag --delete "docker-build-$version"
+    git tag -a "docker-build-$version" -m "docker build version $version"
     git push https://f689da7bc483cddeab89014823f7eb2b49e47fb0:x-oauth-basic@github.com/OXYMENTech/registry.git --tags
 
     echo "Build Version: $version"
